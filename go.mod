3221476e5eb1be1f7da8a0a217f117a2d7a66690
forXuyx <xuyuxiang22@mails.ucas.ac.cn>
2023-03-26 15:30:27 +0800

    新增docker配置，不过网络有点问题

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/go-sql-driver/mysql v1.7.0
 	github.com/golang-jwt/jwt/v4 v4.5.0
 	github.com/jmoiron/sqlx v1.3.5
+	github.com/juju/ratelimit v1.0.2
 	github.com/natefinch/lumberjack v2.0.0+incompatible
 	github.com/spf13/viper v1.15.0
 	github.com/stretchr/testify v1.8.1
@@ -30,6 +31,7 @@ require (
 	github.com/bytedance/sonic v1.8.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gin-contrib/pprof v1.4.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.19.6 // indirect
@@ -39,7 +41,6 @@ require (
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/juju/ratelimit v1.0.2 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
--- routes/routes.go
+++ routes/routes.go
@@ -5,11 +5,9 @@ import (
 	_ "ezTikTok/docs"
 	"ezTikTok/logger"
 	"ezTikTok/middlewares"
+	"github.com/gin-contrib/pprof"
 	"github.com/gin-gonic/gin"
-	swaggerFiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
 	"net/http"
-	"time"
 )
 
 func Setup(mode string) *gin.Engine {
@@ -17,8 +15,14 @@ func Setup(mode string) *gin.Engine {
 		gin.SetMode(gin.ReleaseMode)
 	}
 	r := gin.New()
-	r.Use(logger.GinLogger(), logger.GinRecovery(true), middlewares.RateLimitMiddleware(2*time.Second, 1))
-	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	r.Use(logger.GinLogger(), logger.GinRecovery(true))
+
+	r.LoadHTMLFiles("./templates/index.html")
+	r.Static("/static", "./static")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
 
 	v1 := r.Group("/api/v1")
 
@@ -26,6 +30,11 @@ func Setup(mode string) *gin.Engine {
 	v1.POST("/signup", controller.SignupHandler)
 	// 登录业务路由
 	v1.POST("/login", controller.LoginHandler)
+
+	// 获取帖子列表
+	v1.GET("/posts", controller.GetPostListHandler)
+	// 根据时间或分数获取帖子列表
+	v1.GET("/posts2", controller.GetPostListHandler2)
 
 	v1.Use(middlewares.JWTAuthMiddleware()) // 应用中间件
 	{
@@ -38,13 +47,12 @@ func Setup(mode string) *gin.Engine {
 		v1.POST("/post", controller.CreatePostHandler)
 		// 获取帖子详情
 		v1.GET("/post/:id", controller.GetPostDetailHandler)
-		// 获取帖子列表
-		v1.GET("/posts", controller.GetPostListHandler)
-		// 根据时间或分数获取帖子列表
-		v1.GET("/posts2", controller.GetPostListHandler2)
+
 		// 投票功能
 		v1.POST("/vote", controller.PostVoteHandler)
 	}
+
+	pprof.Register(r)
 
 	r.NoRoute(func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
